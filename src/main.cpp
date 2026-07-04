6a4e99c083b969b566070849b77eb1eaab40ccb1
anushkasharma2005 <anushka.sharma@research.iiit.ac.in>
2026-07-04 18:29:35 +0530

    reading content from the files

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
-
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
-    cout << argv[0] << endl;
-    return 0;
+
+    // just a note that the first argument is the path of the executable itself 
+    // we need the name of the file thats passed as the arg `
+
+
+    if(argc != 2){
+        cerr << "Error: Invalid number of arguments." << endl;
+        cerr << "Correct Usage: " << argv[0] << " <filename.hy>" << endl;
+        return EXIT_FAILURE;
+    }
+
+
+    // cout << "File name: " << argv[1] << endl;
+
+
+    // great now lets actually read the file
+
+    fstream input(argv[1], ios::in); // ios stands for input/output stream, in this case we are opening the file for reading (input)
+    
+    stringstream contents;
+    contents << input.rdbuf(); // read the file into the stringstream, the entire file is in this now 
+
+    input.close(); // close the file, we have the contents in the stringstream now
+
+
+    string contents_str = contents.str(); // convert the stringstream to a string
+
+    cout << "Contents of the file: " << endl;
+    cout << contents_str << endl; // print the contents of the file
+
+
+
+    return EXIT_SUCCESS;
 }
 
 
