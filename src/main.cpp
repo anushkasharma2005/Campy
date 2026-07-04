497a7a1ed065a56230fb73e3f7aeb2b2dc3899db
anushkasharma2005 <anushka.sharma@research.iiit.ac.in>
2026-07-04 22:29:45 +0530

    embracing better practice: removed using namespace std;

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <optional>
 
-using namespace std;
+
+// Define the different types of tokens that can be recognized by the lexer
+enum class TokenType {
+    _return, 
+    int_lit,
+    semi   
+};
+
+struct Token {
+    TokenType type;
+    std::optional<std::string> value;
+};
+
+
 
 int main(int argc, char* argv[]){
 
     // just a note that the first argument is the path of the executable itself 
     // we need the name of the file thats passed as the arg `
-
-
     if(argc != 2){
-        cerr << "Error: Invalid number of arguments." << endl;
-        cerr << "Correct Usage: " << argv[0] << " <filename.hy>" << endl;
+        std::cerr << "Error: Invalid number of arguments." << std::endl;
+        std::cerr << "Correct Usage: " << argv[0] << " <filename.hy>" << std::endl;
         return EXIT_FAILURE;
     }
 
 
     // cout << "File name: " << argv[1] << endl;
-
-
     // great now lets actually read the file
+    // ios stands for input/output stream, in this case we are opening the file for reading (input)
+    std::fstream input(argv[1], std::ios::in); 
+    std::stringstream contents;
+    // read the file into the stringstream, the entire file is in this now 
+    contents << input.rdbuf();
 
-    fstream input(argv[1], ios::in); // ios stands for input/output stream, in this case we are opening the file for reading (input)
-    
-    stringstream contents;
-    contents << input.rdbuf(); // read the file into the stringstream, the entire file is in this now 
+    input.close();
 
-    input.close(); // close the file, we have the contents in the stringstream now
+    // convert the stringstream to a string
+    std::string contents_str = contents.str(); 
 
-
-    string contents_str = contents.str(); // convert the stringstream to a string
-
-    cout << "Contents of the file: " << endl;
-    cout << contents_str << endl; // print the contents of the file
-
+    std::cout << "Contents of the file: " << std::endl;
+    std::cout << contents_str << std::endl; 
 
 
     return EXIT_SUCCESS;
